131f64551531f2699b59f8af0024f4c4ffb9a2e8
Gaube Robert <robert-gabriel.gaube@student.upt.ro>
2022-02-08 20:59:55 +0200

    First draft

--- Problems/urls.py
+++ Problems/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('problem/<slug:slug>', views.problem_page)
+    path('problem/<int:id>', views.problem_page)
 ]
--- Problems/views.py
+++ Problems/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Problem
 
 # Create your views here.
 
-problems = [
-    'prima_prob', '2_prob', '3_prob', '2_prob', '3_prob', '2_prob', '3_prob', '2_prob', '3_prob', '2_prob', '3_prob', '2_prob', '3_prob', '2_prob', '3_prob', '2_prob', '3_prob'
-]
-
 def index(request):
+    problems = Problem.objects.all().order_by("rating")
     return render(request, 'Problems/index.html', {
         'problems': problems
     })
 
-def problem_page(request, slug):
+def problem_page(request, id):
+    problem = get_object_or_404(Problem, pk=id)
     return render(request, 'Problems/problem_page.html', {
-        'title': slug
+        'problem': problem
     })
